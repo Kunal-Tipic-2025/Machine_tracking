5cbcc80b0e50b296ca4bf6df4fe674a0932d3099
samirTipic <samir.sutar@tipic.co.in>
2026-02-05 23:19:30 +0530

    build

--- resources/react/version.js
+++ resources/react/version.js
@@ -1,4 +1,4 @@
 // Auto-generated version file
-export const APP_VERSION = '1.0.47_05_02_2026';
-export const BUILD_DATE = '2026-02-05T14:41:27.758Z';
-export const VERSION_NUMBER = '1.0.47';
+export const APP_VERSION = '1.0.48_05_02_2026';
+export const BUILD_DATE = '2026-02-05T17:43:35.453Z';
+export const VERSION_NUMBER = '1.0.48';
